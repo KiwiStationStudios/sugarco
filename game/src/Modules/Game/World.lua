1a58d46e40610e882f1eae673526812f6a8bcdf4
KiwiSky_ <74114256+itzKiwiSky@users.noreply.github.com>
2025-04-29 23:07:47 -0300

    initial commit

---
+++ game/src/Modules/Game/World.lua
@@ -0,0 +1,123 @@
+local World = {}
+
+local function createTiles(...)
+    local args = {...}
+    local self = args[1]
+
+    -- clear every single tile before rebaking it on the map --
+    self.bth_ground:clear()
+
+    -- create floor tiles --
+    for y = 1, self.h, 1 do
+        for x = 1, self.w, 1 do
+            -- set color based on the position --
+            if x >= self.w or y >= self.h then
+                self.bth_ground:setColor(0.5, 0.5, 0.5, 1)
+            end
+            if x == self.w and y == self.h then
+                self.bth_ground:setColor(0.25, 0.25, 0.25, 1)
+            end
+            self.bth_ground:add(self.assets.walls.quads["ground_stone"], x * self.qw, y * self.qh)
+
+            -- reset --
+            self.bth_ground:setColor(1, 1, 1, 1)
+        end
+    end
+
+    -- generate walls --
+
+    -- corner --
+    self.bth_ground:add(self.assets.walls.quads["wall_corner"], 0, 0)
+    for x = 1, self.w, 1 do
+        -- set color based on the position --
+        if x >= self.w then
+            self.bth_ground:setColor(0.5, 0.5, 0.5, 1)
+        end
+        self.bth_ground:add(self.assets.walls.quads["wall_brick_top"], x * self.qw, 0)
+
+        -- reset --
+        self.bth_ground:setColor(1, 1, 1, 1)
+    end
+
+    for y = 1, self.h, 1 do
+        -- set color based on the position --
+        if y >= self.h then
+            self.bth_ground:setColor(0.5, 0.5, 0.5, 1)
+        end
+        self.bth_ground:add(self.assets.walls.quads["wall_brick_left"], 0, y * self.qh)
+
+        -- reset --
+        self.bth_ground:setColor(1, 1, 1, 1)
+    end
+end
+
+local function updateClickzones(...)
+    local args = {...}
+    local self = args[1]
+
+    self.clickzones["right"].x = self.w * 32
+    self.clickzones["right"].y = 32
+    self.clickzones["right"].h = (self.h - 1) * 32
+
+    self.clickzones["bottom"].x = 32
+    self.clickzones["bottom"].y = self.h * 32
+    self.clickzones["bottom"].w = (self.w - 1) * 32
+end
+
+function World:init(assets, w, h)
+    self.assets = assets
+    self.w = w or 16
+    self.h = h or 16
+
+    self.clickzones = {
+        ["right"] = {
+            x = 0,
+            y = 0,
+            w = 32,
+            h = 32,
+        },
+        ["bottom"] = {
+            x = 0,
+            y = 0,
+            w = 32,
+            h = 32,
+        },
+    }
+    
+    -- create spritebatches --
+    self.bth_ground = love.graphics.newSpriteBatch(self.assets.walls.image, nil, "static")
+    local _, _, qw, qh = self.assets.walls.quads["wall_brick"]:getViewport()
+    self.qw, self.qh = qw, qh
+    createTiles(self)
+    updateClickzones(self)
+
+end
+
+function World:resize()
+    
+end
+
+function World:draw()
+    love.graphics.draw(self.bth_ground, 0, 0)
+    for k, v in pairs(self.clickzones) do
+        love.graphics.rectangle("line", v.x, v.y, v.w, v.h)
+    end
+end
+
+function World:mousepressed(x, y, button)
+    if button == 1 then
+        -- resize shit --
+        for k, v in pairs(self.clickzones) do
+            --love.graphics.rectangle("line", v.x, v.y, v.w, v.h)
+            if collision.pointRect( { x = x, y = y }, v) then
+                if k == "right" then
+                    self.w = self.w + 1
+                    createTiles(self)
+                    updateClickzones(self)
+                end
+            end
+        end
+    end
+end
+
+return World
---
+++ game/src/Scenes/PlayState.lua
@@ -0,0 +1,81 @@
+PlayState = {}
+
+function PlayState:enter()
+    self.world = require 'game.src.Modules.Game.World'
+
+    self.assets = {
+        ["walls"] = {}
+    }
+
+    self.assets.walls.image, self.assets.walls.quads = love.graphics.getQuadsFromHash("assets/images/map/walls_sheet")
+    
+    self.camera = camera.new()
+    self.camera.targetZoom = 1
+    self.camera.zoomSpeed = 0.03
+
+    self.world:init(self.assets, 16, 16)
+end
+
+function PlayState:draw()
+    self.camera:attach()
+        self.world:draw()
+    self.camera:detach()
+end
+
+function PlayState:update(elapsed)
+    self.camera:zoomTo(math.lerp(self.camera.scale, self.camera.targetZoom, 0.1))
+
+    --self.camera.pos.x =
+    local speed = 300
+    if love.keyboard.isDown("a") then
+        self.camera.x = self.camera.x - speed * elapsed
+    end
+    if love.keyboard.isDown("d") then
+        self.camera.x = self.camera.x + speed * elapsed
+    end
+    if love.keyboard.isDown("w") then
+        self.camera.y = self.camera.y - speed * elapsed
+    end
+    if love.keyboard.isDown("s") then
+        self.camera.y = self.camera.y + speed * elapsed
+    end
+
+    self.camera.zoomSpeed = love.keyboard.isDown("lctrl") and 0.3 or 0.03
+
+    if self.camera.targetZoom < 0.5 then
+        self.camera.zoom = 0.5
+        self.camera.targetZoom = 0.5
+    elseif self.camera.targetZoom > 2 then
+        self.camera.zoom = 2
+        self.camera.targetZoom = 2
+    end
+end
+
+function PlayState:keypressed(k)
+
+end
+
+function PlayState:mousepressed(x, y, button)
+    local inside, mx, my = shove.mouseToViewport()
+    local mx, my = self.camera:worldCoords(mx, my)
+
+    self.world:mousepressed(mx, my, button)
+end
+
+function PlayState:wheelmoved(x, y)
+    if y < 0 then
+        self.camera.targetZoom = self.camera.targetZoom - 0.03
+    end
+    if y > 0 then
+        self.camera.targetZoom = self.camera.targetZoom + 0.03
+    end
+end
+
+function PlayState:mousemoved(x, y, dx, dy)
+    if love.mouse.isDown(3) then
+        self.camera.x = self.camera.x - dx / self.camera.scale
+        self.camera.y = self.camera.y - dy / self.camera.scale
+    end
+end
+
+return PlayState
